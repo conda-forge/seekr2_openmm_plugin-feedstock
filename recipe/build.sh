91168536c229172e731f84cf2edcacccc28d8ef9
Lane Votapka <lvotapka100@gmail.com>
2022-03-01 11:10:53 -0700

    Removed script field but added python pip install line to build script.

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+{{ PYTHON }} -m pip install . -vv
+
 CMAKE_ARGS="$CMAKE_ARGS -DCMAKE_BUILD_TYPE=Release -DSEEKR2_BUILD_OPENCL_LIB=OFF -DOPENMM_DIR=$PREFIX"
 
 if [[ "$target_platform" == linux* ]]; then
